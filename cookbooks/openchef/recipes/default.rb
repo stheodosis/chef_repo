7c22f58b370b660ac3aefdf6f7cc91591a4f12f8
Sryros Theodosis <stheodosis@mgage.com>
2015-06-07 16:20:40 +0300

    Chef server init

--- cookbooks/openchef/recipes/default.rb
+++ cookbooks/openchef/recipes/default.rb
@@ -3,3 +3,31 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+# Cookbook Name:: open-chef
+# Recipe:: default
+#
+# Copyright (C) 2014
+#
+#
+#
+
+package_url = node['openchef']['url']
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+# omnibus_package is remote (i.e., a URL) let's download it
+remote_file package_local_path do
+  source package_url
+end
+
+package package_name do
+  source package_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[reconfigure-chef-server]', :immediately
+end
+
+# reconfigure the installation
+execute 'reconfigure-chef-server' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
